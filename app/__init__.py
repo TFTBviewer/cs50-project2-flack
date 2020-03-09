1a432b013138c388914a685d7fae4c7e21f6df4d
tiredfingers <tiredfingers@yandex.ru>
2020-03-09 23:33:56 +0300

    test

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_socketio import SocketIO
 
+#test
+
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret"
 socketio = SocketIO(app)
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from app import app, socketio
 
+#test
+
 if __name__ == "__main__":
     socketio.run(app)
